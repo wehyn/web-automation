6d1432e86596663e69db7d9a292a42727fa08372
wehyn <wayneegarcia@gmail.com>
2023-11-17 22:41:58 +0800

    removed unnecessary libraries

--- saucedemo.py
+++ saucedemo.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 
 def main():
