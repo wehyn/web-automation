746bede87e96b3f035cc10d5c24e510a969c01ea
wehyn <wayneegarcia@gmail.com>
2023-11-17 23:29:14 +0800

    forms

--- ultimateqa.py
+++ ultimateqa.py
@@ -36,6 +36,24 @@ def social_media():
     driver.back()
 
 
+def random_stuff():
+    driver.find_element(By.XPATH, '//*[@id="lpwtoc_widget-2"]/div/div/div[1]/span/a').click()
+    driver.find_element(By.XPATH, '//*[@id="lpwtoc_widget-2"]/div/div/div[1]/span/a').click()
+    driver.find_element(By.XPATH, '//*[@id="et_pb_contact_name_0"]').send_keys('Sauce Demo')
+    driver.find_element(By.XPATH, '//*[@id="et_pb_contact_email_0"]').send_keys('saucedemo@gmail.com')
+    driver.find_element(By.XPATH, '//*[@id="et_pb_contact_message_0"]').send_keys('Nice to meet you!')
+    captcha = driver.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div[2]/form/div/div/p/span')
+    result = eval(captcha.text)
+    driver.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div[2]/form/div/div/p/input').send_keys(result)
+    driver.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div[2]/form/div/button').click()
+
+    driver.find_element(By.NAME, 'log').send_keys('Sauce Demo')
+    driver.find_element(By.NAME, 'pwd').send_keys('secret_sauce')
+    driver.find_element(By.XPATH, '//*[@id="post-579"]/div/div[1]/div/div/div[7]/div[2]/div[2]/div[2]/form/p[4]/button').click()
+    driver.find_element(By.XPATH, '//*[@id="backtoblog"]/a').click()
+
+
 if __name__ == "__main__":
     buttons()
     social_media()
+    random_stuff()
